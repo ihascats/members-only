62f4022eb02d46255989c09a4d0c650869bc23af
ihascats <s.pavlovic.rs@gmail.com>
2022-09-05 21:07:55 +0200

    Create log in form, add log in route

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,18 @@ router.get('/sign-up', function (req, res) {
   res.render('sign-up-form');
 });
 
+router.get('/log-in', function (req, res) {
+  res.render('log-in-form');
+});
+
+router.post('/sign-up', function (req, res) {
+  console.log(req.body);
+  res.redirect('/log-in');
+});
+
+router.post('/log-in', function (req, res) {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 module.exports = router;
