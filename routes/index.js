4b2487ebcb954510a454f9cdb58ea361669c08d5
ihascats <s.pavlovic.rs@gmail.com>
2022-09-05 22:05:47 +0200

    Add sign up validation, excluding password

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 router.get('/', function (req, res, next) {
   res.render('index');
@@ -13,10 +14,23 @@ router.get('/log-in', function (req, res) {
   res.render('log-in-form');
 });
 
-router.post('/sign-up', function (req, res) {
-  console.log(req.body);
-  res.redirect('/log-in');
-});
+router.post(
+  '/sign-up',
+  [
+    body('email').isEmail(),
+    body('username').isLength({ min: 3 }),
+    body('first_name').isLength({ min: 2 }),
+    body('last_name').isLength({ min: 2 }),
+  ],
+  function (req, res) {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log({ errors: errors.array() });
+    }
+    // res.redirect('/log-in');
+  },
+);
 
 router.post('/log-in', function (req, res) {
   console.log(req.body);
